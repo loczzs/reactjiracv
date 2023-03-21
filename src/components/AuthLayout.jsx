3a368f48592ee4700d21ad2be6a96e5f3b8912ca
loczzs <le.baoloczzs@gmail.com>
2023-03-21 04:51:10 -0700

    respon

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -5,15 +5,9 @@ import scss from "./style.module.scss";
 
 const AuthLayout = () => {
   return (
-    <Row>
-      <Col span={12}>
-        <div className={scss.img}>
-        </div>
-      </Col>
-      <Col span={12}>
+    
         <Outlet />
-      </Col>
-    </Row>
+      
   );
 };
 
--- src/modules/Authentication/pages/Login.jsx
+++ src/modules/Authentication/pages/Login.jsx
@@ -42,72 +42,74 @@ const Login = () => {
   }
 
   return (
-    <div className={scss.center}>
-      <h1 className={scss.h1}>Login</h1>
-      <div className={scss.styless}>
-        <Form
-          className={scss.form}
-          onFinish={handleSubmit(onSubmit)}
-          labelCol={{ span: 5 }}
-          wrapperCol={{ span: 8 }}
-        >
-          <Controller
-            name="email"
-            control={control}
-            rules={{
-              required: {
-                value: true,
-                message: "Email không được để trống",
-              },
-            }}
-            render={({ field, fieldState: { error } }) => (
-              <Form.Item
-                className={scss.item}
-                label="Email"
-                validateStatus={error ? "error" : ""}
-                help={error?.message}
-              >
-                <Input type="text" {...field} className={scss.input} />
-              </Form.Item>
-            )}
-          />
+   <div className={scss.center}>
+     <div className={scss.bg}>
+      <div className={scss.contain2}>
+        <h1 className="mb-3 ">Login</h1>
+        <div>
+          <Form
+            onFinish={handleSubmit(onSubmit)}
+          
+          >
+            <Controller
+              name="email"
+              control={control}
+              rules={{
+                required: {
+                  value: true,
+                  message: "Email không được để trống",
+                },
+              }}
+              render={({ field, fieldState: { error } }) => (
+                <Form.Item
+                  className={scss.item}
+                  label="Email"
+                  validateStatus={error ? "error" : ""}
+                  help={error?.message}
+                >
+                  <Input type="text" {...field} className={scss.input} />
+                </Form.Item>
+              )}
+            />
 
-          <Controller
-            className={scss.item}
-            name="passWord"
-            control={control}
-            rules={{
-              required: {
-                value: true,
-                message: "Mật khẩu không được để trống",
-              },
-            }}
-            render={({ field, fieldState: { error } }) => (
-              <Form.Item
-                className={scss.item}
-                label="Mật khẩu"
-                validateStatus={error ? "error" : ""}
-                help={error?.message}
-              >
-                <Input type="password" {...field} />
-              </Form.Item>
-            )}
-          />
+            <Controller
+             
+              name="passWord"
+              control={control}
+              rules={{
+                required: {
+                  value: true,
+                  message: "Mật khẩu không được để trống",
+                },
+              }}
+              render={({ field, fieldState: { error } }) => (
+                <Form.Item
+                style={{ marginBottom: "35px" }}
+                  label="Mật khẩu"
+                  validateStatus={error ? "error" : ""}
+                  help={error?.message}
+                >
+                  <Input type="password" {...field} />
+                </Form.Item>
+              )}
+            />
 
-          <Form.Item wrapperCol={{ offset: 0 }}>
-            <Button
-              type="primary"
-              htmlType="submit"
-            >
-              Đăng Nhập
-            </Button>
-            <a href="" onClick={handleClick}>
-              Đăng ký
-            </a>
-          </Form.Item>
-        </Form>
+            <Form.Item   style={{ textAlign: "center", marginTop: "10px" }}
+              wrapperCol={{ offset: 0 }}>
+              <Button type="primary" htmlType="submit "  disabled={isLoading}
+                loading={isLoading}
+                className="me-3 ">
+                Đăng Nhập
+              </Button>
+              <a href="" onClick={handleClick}>
+                Đăng ký
+              </a>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </div>
+   </div>
   );
 };
 
